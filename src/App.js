a7a142bf2e5e23a168f1c3a97c8f1796b7871629
Caleb Morales <caleb.morales@brightbridgeweb.com>
2020-10-07 22:16:39 -0600

    it's broken and I have no idea why

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Quote from './components/quote';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+import axios from 'axios';
+
+class App extends Component {
+  state = {
+    quote: []
+  }
+
+  componentDidMount() {
+    axios.get('https://officeapi.dev/api/quotes/random')
+      .then(response => {
+        const quote = response.data;
+        this.setState({ quote });
+      })
+  }
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {isToggleOn: false};
+  //   this.handleClick = this.handleClick.bind(this);
+  // }
+
+  // handleClick() {
+  //   this.setState(prevState => ({
+  //     isToggleOn: !prevState.isToggleOn
+  //   }));
+  // }
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>Hello, World!</h1>
+          {/* <button onClick={this.handleClick}>
+            {this.state.isToggleOn ? 'Boop' : 'Click Me'}
+          </button> */}
+        </div>
+        <Quote quote={this.state.quote} />
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
